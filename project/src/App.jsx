29dcb67106a1d0b95ca368a2f0f13064c92a0c7e
Sahil Sambyal <rajputsahilsambyals@gmail.com>
2022-09-05 07:11:39 +0530

    First Commit

---
+++ project/src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+import SignInOutContainer from './Containers'
+const App = () => {
+    return (
+        <div className='App'>
+    
+            <SignInOutContainer />
+
+        </div>
+    )
+}
+
+export default App
